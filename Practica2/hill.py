cc9744e3f829b5440984c20450e318f8cd1138b9
Victor Molina <victor98@ciencias.unam.mx>
2020-02-26 20:22:23 -0600

    Descarga P2

---
+++ Practica2/hill.py
@@ -0,0 +1,30 @@
+class Hill():
+
+    def __init__(self, alphabet, n, key=None):
+        """
+        Constructor de clase, recibiendo un alfabeto completamente necesario pero
+        podría no recibir una llave de cifrado, en cuyo caso, hay que generar una,
+        para el caso del tamañHo de la llave, hay que asegurarse que tiene raíz entera.
+        :param alphabet: una cadena con todos los elementos del alfabeto.
+        :param n: el tamaño de la llave, obligatorio siempre.
+        :param key: una cadena que corresponde a la llave, en caso de ser una llave inválida
+        arrojar una CryptographyException.
+        """
+
+    def cipher(self, message):
+        """
+        Aplica el algoritmo de cifrado con respecto al criptosistema de Hill, el cual recordando
+        que todas las operaciones son mod |alphabet|.
+        :param message: El mensaje a enviar que debe ser cifrado.
+        :return: Un criptotexto correspondiente al mensaje, este debe de estar en representación de
+        cadena, no lista.
+        """
+
+
+    def decipher(self, ciphered):
+        """
+        Usando el algoritmo de decifrado, recibiendo una cadena que se asegura que fue cifrada
+        previamente con el algoritmo de Hill, obtiene el texto plano correspondiente.
+        :param ciphered: El criptotexto de algún mensaje posible.
+        :return: El texto plano correspondiente a manera de cadena.
+        """
