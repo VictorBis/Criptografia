39b9067f0060295e7f9a7a0820cd22d568ae5272
Victor Molina <victor98@ciencias.unam.mx>
2020-05-19 16:23:27 -0500

    Prime generator terminado

---
+++ Practica6/prime_generator.py
@@ -0,0 +1,66 @@
+from random import randint
+from random import randrange
+from utils import factorial
+
+
+def big_int(size=None):
+    """
+    Generador de números aleatorios de un tamaño fijo recibido como parámetro, si el parámetro es
+    menor que 100 o None, entonces la función no le hace caso y genera uno de tamaño arbitrario,
+    máximo es de 150 dígitos.
+    :return: Un número del tamaño descrito.
+    """
+    if size is None or size < 100:
+        size = randint(100, 150)
+    return randint(10**(size-1), (10**size)-1)
+
+
+def miller_rabin(n):
+    """
+    Implementación del test de primalidad de Miller-Rabin.
+    :param n: El número a determinar su primalidad.
+    :return: True si n es primo, False en otro caso.
+    """
+    if n == 2 or n == 3:
+        return True
+    k = 2
+    r, d = 0, n-1
+    while d % 2 == 0:  # Factorizar las potencias de 2 de n-2
+        d //= 2
+        r += 1
+
+    for _ in range(k):
+        a = randrange(2, n-1)
+        x = pow(a, d, n)
+        if x != 1 and x != n-1:
+            for _ in range(r-1):
+                x = pow(x, 2, n)
+                if x == n-1:
+                    break
+            else:
+                return False
+    return True
+
+
+def wilson(n):
+    """
+    Implementaión del test de primalidad de Wilson, basado en el teorema de Wilson,
+    (p-1)! ≡ -1 mod p
+    :param n: El número a determinar su primalidad.
+    :return: True si n es primo, False en otro caso.
+    """
+    return fac(n-1) % n+1 == n
+
+
+def generate_prime(size=None):
+    """
+    Genera un primo de al menos $size dígitos, si no se especifica,
+    este tiene que asegurar que al menos tiene 100 dígitos.
+
+    :param size: El tamaño del primo a generar.
+    :return: Un número que se asegura que es primo.
+    """
+    n = big_int(size)
+    while not miller_rabin(n):
+        n = big_int(size)
+    return n
---
+++ Practica6/utils.py
@@ -0,0 +1,38 @@
+def prime_relative(a, b):
+    if(b == 0):
+        return a == 1
+    else:
+        return prime_relative(b, a % b)
+
+
+def factorial(n):
+    """
+Calcula el factorial de un numero
+Se calcula el factorial reduciendo la multiplicacion en factoriales a la mitad
+
+Para mas informacion acerca del algoritmo, consultar:
+https://sites.google.com/site/examath/research/factorials
+    """
+    if n == 1 or n == 0:
+        return 1
+
+    is_odd = False
+    upto_n = n
+
+    if n & 1 == 1:
+        upto_n -= 1
+        is_odd = True
+
+    next_sum = upto_n
+    next_multi = upto_n
+    factorial = 1
+
+    while next_sum >= 2:
+        factorial *= next_multi
+        next_sum -= 2
+        next_multi += next_sum
+
+    if is_odd:
+        factorial *= n
+
+    return factorial
